ced1a111f71ae1539e67d3a359e5951ff43b6bbc
MalekTouati <touatimalek2005@gmail.com>
2019-07-19 14:37:22 +0100

    haard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Wrapper from './Wrapper';
+import Photo from './Photo';
+import Title from './Title';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper>
+        <Photo src="/profile.png" />
+        <Title style={{ color : 'red'}}>My Name here</Title>
+        <Title small>My job here</Title>
+      </Wrapper>
     </div>
-  );
+  )
 }
 
 export default App;
